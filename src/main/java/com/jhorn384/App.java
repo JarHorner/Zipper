a2e24ece3c185cec981425cecb200247cd015346
Jarret Horner <JHorn384@mtroyal.ca>
2023-05-03 22:22:55 -0600

    Restructuring Application to use CSS files, instead of putting CSS in .fxml files

--- src/main/java/com/jhorn384/App.java
+++ src/main/java/com/jhorn384/App.java
@@ -2,11 +2,16 @@ package com.jhorn384;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+
 
 /**
  * JavaFX App
@@ -17,8 +22,29 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 960, 720);
 
+        Circle circle = new Circle();
+        circle.setCenterX(100.0f);
+        circle.setCenterY(100.0f);
+        circle.setRadius(50.0f);
+
+        Group g = new Group();
+        g.getChildren().add(circle);
+
+        Pane bg = new Pane();
+        bg.setId("bg");
+        bg.getChildren().add(g);
+
+        scene = new Scene(bg, 640, 480);
+
+        URL url = this.getClass().getResource("Stylesheet.css");
+        if (url == null) {
+            System.out.println("Resource not found. Aborting.");
+            System.exit(-1);
+        }
+        String css = url.toExternalForm(); 
+        scene.getStylesheets().add(css);
+        
         stage.setTitle("Zipper");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/jhorn384/PrimaryController.java
+++ src/main/java/com/jhorn384/PrimaryController.java
@@ -11,8 +11,42 @@ import javafx.stage.DirectoryChooser;
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private void selectFileToUnzip() throws IOException {
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        // sets title of file explorer and its starting location
+        directoryChooser.setTitle("Select Directory");
+        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        // Opens the window
+        File selectedDirectory = directoryChooser.showDialog(null);
+        // if directory is selected gets its absolute path, and starts to zip it
+        if (selectedDirectory != null) {
+            String directoryPath = selectedDirectory.getAbsolutePath();
+            System.out.println("The file at " + directoryPath + " is being zipped!");
+            System.out.println("----------------------------------");
+
+            int beginIndex = directoryPath.lastIndexOf('\\');
+            String fileName = directoryPath.substring(beginIndex + 1);
+            System.out.println(fileName);
+
+            // trys to zip file, if the file exists
+            try {
+                FileOutputStream fos = new FileOutputStream(directoryPath + ".zip");
+                ZipOutputStream zipOut = new ZipOutputStream(fos);
+
+                File fileToZip = new File(directoryPath);
+
+                ZipFile.zipFile(fileToZip, fileName, zipOut);
+
+                System.out.println("----------------------------------");
+                System.out.println("The file is zipped!");
+                System.out.println("\n");
+
+                // fos.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
     }
 
     @FXML
