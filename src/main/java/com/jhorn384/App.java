631d5eb2b90613e247581a21de97718103073e97
Jarret Horner <JHorn384@mtroyal.ca>
2023-05-04 20:00:47 -0600

    Got a decent GUI, and both zipping and unzipping work properly!

--- src/main/java/com/jhorn384/App.java
+++ src/main/java/com/jhorn384/App.java
@@ -2,16 +2,18 @@ package com.jhorn384;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-
 
 /**
  * JavaFX App
@@ -23,28 +25,83 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        Circle circle = new Circle();
-        circle.setCenterX(100.0f);
-        circle.setCenterY(100.0f);
-        circle.setRadius(50.0f);
+        // vars as these values are used in multiple elements
+        double buttonHeight = 100;
+        double buttonWidth = 300;
 
-        Group g = new Group();
-        g.getChildren().add(circle);
+        // Create an instance of PrimaryController to operate event listeners of buttons
+        PrimaryController controller = new PrimaryController();
 
-        Pane bg = new Pane();
-        bg.setId("bg");
-        bg.getChildren().add(g);
+        // grid to properly place each element
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_CENTER);
+        grid.setHgap(10.0);
+        grid.setVgap(20.0);
+        grid.setId("grid");
 
-        scene = new Scene(bg, 640, 480);
+        // title of app
+        Text title = new Text("Zipper");
+        title.setId("title");
 
+        // put the title in an Horizontal box, so the text can be center aligned
+        HBox titleBox = new HBox();
+        titleBox.setAlignment(Pos.CENTER);
+        titleBox.getChildren().add(title);
+
+        // button to zip a file
+        Button zipButton = new Button("Zip File");
+        zipButton.setId("zipButton");
+
+        zipButton.setOnAction(event -> {
+            // Call the selectFileToZip() method of PrimaryController
+            try {
+                controller.selectFileToZip();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        // button to unzip a file
+        Button unzipButton = new Button("Unzip File");
+        unzipButton.setId("unzipButton");
+
+        unzipButton.setOnAction(event -> {
+            // Call the handleButtonClick() method of PrimaryController
+            try {
+                controller.selectFileToUnzip();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        // adds each element to the grid
+        grid.add(titleBox, 0, 0);
+        grid.add(zipButton, 0, 1);
+        grid.add(unzipButton, 0, 2);
+
+        // sets the prefered width and height of each element
+        titleBox.setPrefWidth(buttonWidth);
+        titleBox.setPrefHeight(buttonHeight);
+
+        zipButton.setPrefWidth(buttonWidth);
+        zipButton.setPrefHeight(buttonHeight);
+
+        unzipButton.setPrefWidth(buttonWidth);
+        unzipButton.setPrefHeight(buttonHeight);
+
+        // creates the displayed scene at a specified size
+        scene = new Scene(grid, 440, 360);
+
+        // this ensures a CSS file is loaded for styling
         URL url = this.getClass().getResource("Stylesheet.css");
         if (url == null) {
             System.out.println("Resource not found. Aborting.");
             System.exit(-1);
         }
-        String css = url.toExternalForm(); 
+        String css = url.toExternalForm();
         scene.getStylesheets().add(css);
-        
+
+        // sets title of the app and shows the scene
         stage.setTitle("Zipper");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/jhorn384/PrimaryController.java
+++ src/main/java/com/jhorn384/PrimaryController.java
@@ -1,21 +1,25 @@
 package com.jhorn384;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.awt.Desktop;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 import javafx.fxml.FXML;
 import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 
 public class PrimaryController {
 
     @FXML
-    private void selectFileToUnzip() throws IOException {
+    void selectFileToZip() throws IOException {
 
         DirectoryChooser directoryChooser = new DirectoryChooser();
         // sets title of file explorer and its starting location
-        directoryChooser.setTitle("Select Directory");
+        directoryChooser.setTitle("Select Directory to zip");
         directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
         // Opens the window
         File selectedDirectory = directoryChooser.showDialog(null);
@@ -42,48 +46,57 @@ public class PrimaryController {
                 System.out.println("The file is zipped!");
                 System.out.println("\n");
 
-                // fos.close();
+                fos.close();
             } catch (Exception e) {
-                System.out.println(e);
+                throw new IOException("Failed to zip this file at " + directoryPath);
             }
         }
     }
 
     @FXML
-    private void selectFileToZip() throws IOException {
+    void selectFileToUnzip() throws IOException {
 
-        DirectoryChooser directoryChooser = new DirectoryChooser();
+        FileChooser fileChooser = new FileChooser();
         // sets title of file explorer and its starting location
-        directoryChooser.setTitle("Select Directory");
-        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.setTitle("Select Directory to unzip");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+
+        // Set the file extension filter to show only zip files
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("ZIP files (*.zip)", "*.zip");
+        fileChooser.getExtensionFilters().add(extFilter);
+
         // Opens the window
-        File selectedDirectory = directoryChooser.showDialog(null);
+        File selectedFile = fileChooser.showOpenDialog(null);
+
         // if directory is selected gets its absolute path, and starts to zip it
-        if (selectedDirectory != null) {
-            String directoryPath = selectedDirectory.getAbsolutePath();
-            System.out.println("The file at " + directoryPath + " is being zipped!");
+        if (selectedFile != null) {
+            String directoryPath = selectedFile.getAbsolutePath();
+
+            System.out.println("The file at " + directoryPath + " is being unzipped!");
             System.out.println("----------------------------------");
 
             int beginIndex = directoryPath.lastIndexOf('\\');
-            String fileName = directoryPath.substring(beginIndex + 1);
-            System.out.println(fileName);
+            int endIndex = directoryPath.lastIndexOf('.');
+            String path = directoryPath.substring(0, beginIndex + 1);
 
             // trys to zip file, if the file exists
             try {
-                FileOutputStream fos = new FileOutputStream(directoryPath + ".zip");
-                ZipOutputStream zipOut = new ZipOutputStream(fos);
+                File pathToUnzip = new File(path);
+                ZipInputStream zipIn = new ZipInputStream(new FileInputStream(directoryPath));
 
-                File fileToZip = new File(directoryPath);
+                UnzipFile.unzipFile(pathToUnzip, zipIn);
 
-                ZipFile.zipFile(fileToZip, fileName, zipOut);
+                String fileToOpen = directoryPath.substring(0, endIndex + 1);
+                Desktop desktop = Desktop.getDesktop();
+
+                desktop.open(new File(fileToOpen));
 
                 System.out.println("----------------------------------");
                 System.out.println("The file is zipped!");
                 System.out.println("\n");
 
-                // fos.close();
             } catch (Exception e) {
-                System.out.println(e);
+                throw new IOException("Failed to unzip this file at " + directoryPath);
             }
         }
     }
